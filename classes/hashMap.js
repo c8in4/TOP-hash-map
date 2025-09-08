df1f1f44fa690c3474cd24ef2ad362ae21307be9
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-09-08 21:59:42 +1200

    Implement get method for hashMap

---
+++ classes/hashMap.js
@@ -0,0 +1,82 @@
+import LinkedList from "./linkedList.js";
+export default class HashMap {
+  constructor(loadFactor, capacity) {
+    this.capacity = capacity;
+    this.content = [];
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
+
+  set(key, value) {
+    // if key already exists, overwrite old value
+    // dealing with collisions using linked lists
+    const index = this.hash(key);
+    const data = { key, value };
+
+    if (!this.content[index]) {
+      const newLinkedList = new LinkedList();
+      newLinkedList.append(data);
+      this.content[index] = newLinkedList;
+    } else {
+      this.content[index].append(data);
+    }
+  }
+
+  get(key) {
+    // return value or null
+    const hashMapIndex = this.hash(key);
+    const linkedListIndex = this.content[hashMapIndex]
+      ? this.content[hashMapIndex].find(key)
+      : null;
+
+    return linkedListIndex == null
+      ? null
+      : this.content[hashMapIndex].at(linkedListIndex).value;
+  }
+
+  has(key) {
+    // return true or false
+    const hashMapIndex = this.hash(key);
+    return this.content[hashMapIndex]
+      ? this.content[hashMapIndex].contains(key)
+      : false;
+  }
+
+  remove(key) {
+    // remove entry and return true or return false
+  }
+
+  length() {
+    // return the number of stored keys
+  }
+
+  clear() {
+    // remove all entries
+  }
+
+  keys() {
+    // return an array of all keys
+  }
+
+  values() {
+    // return an array of all the values
+  }
+
+  entries() {
+    // return an array that contains all 'key, value' pairs
+    // format: [[firstKey, firstValue], [secondKey, secondValue]]
+  }
+}
+
+// snippet to limit hashMap size
+// if (index < 0 || index >= buckets.length) {
+//   throw new Error("Trying to access index out of bounds");
+// }
---
+++ script.js
@@ -0,0 +1,127 @@
+import HashMap from "./classes/hashMap.js";
+
+const newHashMap = new HashMap(0.8, 16);
+
+const testStrings = [
+  "VAQzNHxGZc",
+  "cnaJrADYfY",
+  "nDZQceOtwV",
+  "IKuZKMlIwn",
+  "JPxnsUePAX",
+  "XqMLNJDntQ",
+  "WIsZfgAWYe",
+  "yDyvQqsVfg",
+  "yxGjTJskqm",
+  "KONhEISnUG",
+  "TTEWDzzvZl",
+  "evCSkJBYrk",
+  "GllTeDZQqj",
+  "cIxEqtsvbL",
+  "raNslUCcpM",
+  "IgKwqOpvTW",
+  "nYJyBHJxtD",
+  "AjjmHrmahX",
+  "saEHCLwGtw",
+  "hKQHeulnFa",
+  "GMRmgwSyxx",
+  "UtqOKnPTID",
+  "JEVogIYlTk",
+  "LejIQhTTTu",
+  "IhTpidONQP",
+  "YWbjrHBEVF",
+  "KHHREbMqpH",
+  "koXuZEphTL",
+  "oQAtnFOCZs",
+  "nMyySZnEUl",
+  "UglSQPPObQ",
+  //   "kJPGGMYarr",
+  //   "RkFQFPuFVz",
+  //   "ICgCOVbbSW",
+  //   "rdIhqlHSZY",
+  //   "IKKWoniPaq",
+  //   "tbgTtxbyob",
+  //   "QsIaGInBLJ",
+  //   "YLvknuDEho",
+  //   "lYVkAibADh",
+  //   "qERPuVFgxS",
+  //   "MZFCiDXwnV",
+  //   "AbPoZjZGAi",
+  //   "ObIbBhQqwR",
+  //   "ylRLiMxxqL",
+  //   "seAtjCBtPu",
+  //   "ANwlABsgss",
+  //   "owcIREnXxm",
+  //   "pYdCitAFoW",
+  //   "DxwahFCDHn",
+  //   "EFKFnXbuhi",
+  //   "uDrGpbsEaD",
+  //   "aSryroHkeO",
+  //   "XHIKGZMwdo",
+  //   "oLJiEQJggd",
+  //   "ZlKGljShDd",
+  //   "tOMpiVanQO",
+  //   "GIKpPtFXZq",
+  //   "XBMCGYjFJU",
+  //   "vpsKOBKeVX",
+  //   "PIzNistcbV",
+  //   "SfSGnjTFjA",
+  //   "YYwObcxwvq",
+  //   "cuaEHcKeoH",
+  //   "IlVvpTvJIT",
+  //   "kxUUJzlAyE",
+  //   "NyBdWDWKXO",
+  //   "GDAcsSpPwi",
+  //   "eShBkYTxUd",
+  //   "rdyQlouzeB",
+  //   "ylVxXbNskI",
+  //   "WdjNSjItGO",
+  //   "LYEYVrcAKJ",
+  //   "GhwSfTjKOq",
+  //   "AcHJERHMnn",
+  //   "wFZZDNPRYS",
+  //   "GLwFiyfleP",
+  //   "ezqgqvAcxT",
+  //   "YVLuZRUsdx",
+  //   "OZvOjrUXGF",
+  //   "BVGguBGdbr",
+  //   "jsQulfckxP",
+  //   "XHMqUCjGdA",
+  //   "pBzQuRqKWA",
+  //   "MPjCIUFkue",
+  //   "RrrWNAaKOE",
+  //   "SfEZucVxFe",
+  //   "NemIQuhYet",
+  //   "vxBFtGqosf",
+  //   "tullLhGuTj",
+  //   "NivlMkfuwl",
+  //   "oEwpfMRIqn",
+  //   "GhTdluMjWA",
+  //   "wOLtWFiJSk",
+  //   "kfldgWJrpc",
+  //   "RHRpJMdnuj",
+  //   "DASQnqYiZN",
+  //   "UXBJCIxSaD",
+  //   "ujHvLuoNQk",
+  //   "bPfofCrAJs",
+];
+
+let i = 0;
+testStrings.forEach((str) => {
+  newHashMap.set(str, `item ${i}`);
+  i++;
+});
+
+// // console.log("HashMap content:", newHashMap.content[5]);
+// newHashMap.content.forEach((element) => {
+//   //   console.log(element.size());
+// });
+
+// console.log(i);
+
+// console.log("nYJyBHJxtD");
+
+// console.log(newHashMap.content[91]);
+
+console.log("HashMap content:", newHashMap.get("nYJyBHJxtD"));
+
+// console.log(newHashMap.get("eShBkYTxUd"));
