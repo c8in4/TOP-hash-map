eedc49a9b0b717b4950aae5ee133699dfe8ff143
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-09-09 18:46:24 +1200

    Implement has() and set()

--- classes/hashMap.js
+++ classes/hashMap.js
@@ -1,4 +1,3 @@
-import LinkedList from "./linkedList.js";
 export default class HashMap {
   constructor(loadFactor, capacity) {
     this.loadFactor = loadFactor;
@@ -6,19 +5,18 @@ export default class HashMap {
     this.content = [];
   }
 
-  checkForResize() {
-    const resize = this.loadFactor * this.capacity;
-    if (this.length() + 1 > resize) {
+  checkCapacity() {
+    if (this.length() > this.loadFactor * this.capacity) {
       this.capacity *= 2;
-      // console.log("new capacity", this.capacity);
 
-      const currentElements = this.values();
-      this.clear();
-      currentElements.forEach((element) => {
-        this.set(element.key, element);
-      });
-      // console.log("capacity doubled and elements copied");
-      return;
+      //   // console.log("new capacity", this.capacity);
+      //   const currentElements = this.values();
+      //   this.clear();
+      //   currentElements.forEach((element) => {
+      //     this.set(element.key, element);
+      //   });
+      //   // console.log("capacity doubled and elements copied");
+      //   return;
     }
   }
 
@@ -32,65 +30,55 @@ export default class HashMap {
     return hashCode;
   }
 
-  // dealing with collisions using linked lists
-  // TODO: if key already exists, overwrite old value
+  // dealing with collisions?
+  // if key already exists, overwrite old value
   set(key, value) {
-    const index = this.hash(key);
-    const data = { key, value };
-    this.checkForResize();
-    if (!this.content[index]) {
-      const newLinkedList = new LinkedList();
-      newLinkedList.append(data);
-      this.content[index] = newLinkedList;
+    const bucketIndex = this.hash(key);
+    const data = { [key]: value };
+
+    if (!this.content[bucketIndex]) {
+      this.content[bucketIndex] = data;
     } else {
-      this.content[index].append(data);
+      this.content[bucketIndex][key] = value;
     }
-    // return console.log(`${value} set`);
   }
 
   // return value or null
   get(key) {
-    const hashMapIndex = this.hash(key);
-    const linkedListIndex = this.content[hashMapIndex]
-      ? this.content[hashMapIndex].find(key)
-      : null;
-
-    return linkedListIndex == null
-      ? null
-      : this.content[hashMapIndex].at(linkedListIndex).value;
+    // const hashMapIndex = this.hash(key);
+    // const linkedListIndex = this.content[hashMapIndex]
+    //   ? this.content[hashMapIndex].find(key)
+    //   : null;
+    // return linkedListIndex == null
+    //   ? null
+    //   : this.content[hashMapIndex].at(linkedListIndex).value;
   }
 
   // return true or false
   has(key) {
     const hashMapIndex = this.hash(key);
-    return this.content[hashMapIndex]
-      ? this.content[hashMapIndex].contains(key)
-      : false;
+    return key in this.content[hashMapIndex];
   }
 
   // remove entry and return true or return false
   remove(key) {
-    if (!this.has(key)) return false;
-    // console.log("can not remove item - item does not exist");
-
-    const hashMapIndex = this.hash(key);
-    const linkedListIndex = this.content[hashMapIndex].find(key);
-
-    this.content[hashMapIndex].removeAt(linkedListIndex);
-    // console.log(this.content[hashMapIndex].at(linkedListIndex));
-
-    // console.log("item has been removed");
-
-    return true;
+    // if (!this.has(key)) return false;
+    // // console.log("can not remove item - item does not exist");
+    // const hashMapIndex = this.hash(key);
+    // const linkedListIndex = this.content[hashMapIndex].find(key);
+    // this.content[hashMapIndex].removeAt(linkedListIndex);
+    // // console.log(this.content[hashMapIndex].at(linkedListIndex));
+    // // console.log("item has been removed");
+    // return true;
   }
 
   // return the number of stored keys
   length() {
-    let length = 0;
-    this.content.forEach((bucket) => {
-      if (bucket) length += bucket.size();
-    });
-    return length;
+    // let length = 0;
+    // this.content.forEach((bucket) => {
+    //   if (bucket) length += bucket.size();
+    // });
+    // return length;
   }
 
   // remove all entries
@@ -102,47 +90,45 @@ export default class HashMap {
 
   // return an array of all keys
   keys() {
-    const keys = [];
-    this.content.forEach((bucket) => {
-      if (!bucket) return;
-      const objects = bucket.toArray();
-      if (!objects) return;
-      objects.forEach((object) => {
-        if (!object) return;
-        keys.push(object.key);
-      });
-    });
-
-    return keys;
+    // const keys = [];
+    // this.content.forEach((bucket) => {
+    //   if (!bucket) return;
+    //   const objects = bucket.toArray();
+    //   if (!objects) return;
+    //   objects.forEach((object) => {
+    //     if (!object) return;
+    //     keys.push(object.key);
+    //   });
+    // });
+    // return keys;
   }
 
   // return an array of all the values
   values() {
-    const values = [];
-    this.content.forEach((bucket) => {
-      if (!bucket) return;
-      const objects = bucket.toArray();
-      if (!objects) return;
-      objects.forEach((object) => {
-        if (!object) return;
-        values.push(object.value);
-      });
-    });
-    return values;
+    // const values = [];
+    // this.content.forEach((bucket) => {
+    //   if (!bucket) return;
+    //   const objects = bucket.toArray();
+    //   if (!objects) return;
+    //   objects.forEach((object) => {
+    //     if (!object) return;
+    //     values.push(object.value);
+    //   });
+    // });
+    // return values;
   }
 
   // return an array that contains all 'key, value' pairs
   // format: [[firstKey, firstValue], [secondKey, secondValue]]
   entries() {
-    const entries = [];
-    const keys = this.keys();
-    const values = this.values();
-    for (let i = 0; i < keys.length; i++) {
-      const newEntry = [keys[i], values[i]];
-      entries.push(newEntry);
-    }
-
-    return entries;
+    // const entries = [];
+    // const keys = this.keys();
+    // const values = this.values();
+    // for (let i = 0; i < keys.length; i++) {
+    //   const newEntry = [keys[i], values[i]];
+    //   entries.push(newEntry);
+    // }
+    // return entries;
   }
 }
 
--- script.js
+++ script.js
@@ -1,166 +1,51 @@
 import HashMap from "./classes/hashMap.js";
 
-const newHashMap = new HashMap(0.8, 16);
-
-const testStrings = [
-  "VAQzNHxGZc",
-  "cnaJrADYfY",
-  "nDZQceOtwV",
-  "IKuZKMlIwn",
-  "JPxnsUePAX",
-  "XqMLNJDntQ",
-  "WIsZfgAWYe",
-  "yDyvQqsVfg",
-  "yxGjTJskqm",
-  "KONhEISnUG",
-  "TTEWDzzvZl",
-  "evCSkJBYrk",
-  "GllTeDZQqj",
-  "cIxEqtsvbL",
-  "raNslUCcpM",
-  "IgKwqOpvTW",
-  "nYJyBHJxtD",
-  "AjjmHrmahX",
-  "saEHCLwGtw",
-  "hKQHeulnFa",
-  "GMRmgwSyxx",
-  "UtqOKnPTID",
-  "JEVogIYlTk",
-  "LejIQhTTTu",
-  "IhTpidONQP",
-  "YWbjrHBEVF",
-  "KHHREbMqpH",
-  "koXuZEphTL",
-  "oQAtnFOCZs",
-  "nMyySZnEUl",
-  "UglSQPPObQ",
-  "kJPGGMYarr",
-  "RkFQFPuFVz",
-  "ICgCOVbbSW",
-  "rdIhqlHSZY",
-  "IKKWoniPaq",
-  "tbgTtxbyob",
-  "QsIaGInBLJ",
-  "YLvknuDEho",
-  "lYVkAibADh",
-  "qERPuVFgxS",
-  "MZFCiDXwnV",
-  "AbPoZjZGAi",
-  "ObIbBhQqwR",
-  "ylRLiMxxqL",
-  "seAtjCBtPu",
-  "ANwlABsgss",
-  "owcIREnXxm",
-  "pYdCitAFoW",
-  "DxwahFCDHn",
-  "EFKFnXbuhi",
-  "uDrGpbsEaD",
-  "aSryroHkeO",
-  "XHIKGZMwdo",
-  "oLJiEQJggd",
-  "ZlKGljShDd",
-  "tOMpiVanQO",
-  "GIKpPtFXZq",
-  "XBMCGYjFJU",
-  "vpsKOBKeVX",
-  "PIzNistcbV",
-  "SfSGnjTFjA",
-  "YYwObcxwvq",
-  "cuaEHcKeoH",
-  "IlVvpTvJIT",
-  "kxUUJzlAyE",
-  "NyBdWDWKXO",
-  "GDAcsSpPwi",
-  "eShBkYTxUd",
-  "rdyQlouzeB",
-  "ylVxXbNskI",
-  "WdjNSjItGO",
-  "LYEYVrcAKJ",
-  "GhwSfTjKOq",
-  "AcHJERHMnn",
-  "wFZZDNPRYS",
-  "GLwFiyfleP",
-  "ezqgqvAcxT",
-  "YVLuZRUsdx",
-  "OZvOjrUXGF",
-  "BVGguBGdbr",
-  "jsQulfckxP",
-  "XHMqUCjGdA",
-  "pBzQuRqKWA",
-  "MPjCIUFkue",
-  "RrrWNAaKOE",
-  "SfEZucVxFe",
-  "NemIQuhYet",
-  "vxBFtGqosf",
-  "tullLhGuTj",
-  "NivlMkfuwl",
-  "oEwpfMRIqn",
-  "GhTdluMjWA",
-  "wOLtWFiJSk",
-  "kfldgWJrpc",
-  "RHRpJMdnuj",
-  "DASQnqYiZN",
-  "UXBJCIxSaD",
-  "ujHvLuoNQk",
-  "bPfofCrAJs",
-];
-
-function createMockObjects(arrayOfStrings) {
-  const data = [];
-  let i = 1;
-  arrayOfStrings.forEach((str) => {
-    data.push({ key: str, value: `item ${i}` });
-    i++;
-  });
-  return data;
-}
-
-const mockData = createMockObjects(testStrings);
-
-// set(key, value)
-mockData.forEach((object) => newHashMap.set(object.key, object));
-
-// get(key)
-console.log("Get existing key:", newHashMap.get("pBzQuRqKWA"));
-console.log("Get non-existing key:", newHashMap.get("pBzQuRqKW"));
-
-// has(key)
-console.log("Has:", newHashMap.has("pBzQuRqKWA"));
-console.log("Has not:", newHashMap.has("pBzQuRqKW"));
-
-// remove(key)
-console.log("remove item IlVvpTvJIT:", newHashMap.remove("IlVvpTvJIT"));
-console.log("remove item GIKpPtFXZq:", newHashMap.remove("GIKpPtFXZq"));
-console.log("remove non-existing item", newHashMap.remove("pBzQuRqKW"));
-
-// length()
-console.log("HashMap length:", newHashMap.length());
-
-// clear()
-// newHashMap.clear();
-
-// keys()
-console.log("HashMap keys:", newHashMap.keys());
-
-// values()
-console.log("HashMap values:", newHashMap.values());
-
-// entries()
-console.log("HashMap entries:", newHashMap.entries());
-
 // The Odin Project - test
 const test = new HashMap(0.8, 8);
-test.set("apple", { key: "apple", value: "red" });
-test.set("banana", { key: "banana", value: "yellow" });
-test.set("carrot", { key: "carrot", value: "orange" });
-test.set("dog", { key: "dog", value: "brown" });
-test.set("elephant", { key: "elephant", value: "gray" });
-test.set("frog", { key: "frog", value: "green" });
-test.set("grape", { key: "grape", value: "purple" });
-test.set("hat", { key: "hat", value: "black" });
-test.set("ice cream", { key: "ice cream", value: "white" });
-test.set("jacket", { key: "jacket", value: "blue" });
-test.set("kite", { key: "kite", value: "pink" });
-test.set("lion", { key: "lion", value: "golden" });
+test.set("apple", "red");
+// test.set("apple", "green");
 
-// console.log(test);
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+console.log(test.content);
+
+// set(key, value)
+// mockData.forEach((object) => test.set(object.key, object));
+
+// get(key)
+// console.log("Get existing key:", test.get("pBzQuRqKWA"));
+// console.log("Get non-existing key:", test.get("pBzQuRqKW"));
+
+// has(key)
+// console.log("Has:", test.has("carrot"));
+// console.log("Has not:", test.has("pBzQuRqKW"));
+
+// remove(key)
+// console.log("remove item IlVvpTvJIT:", test.remove("IlVvpTvJIT"));
+// console.log("remove item GIKpPtFXZq:", test.remove("GIKpPtFXZq"));
+// console.log("remove non-existing item", test.remove("pBzQuRqKW"));
+
+// length()
+// console.log("HashMap length:", test.length());
+
+// clear()
+// test.clear();
+
+// keys()
+// console.log("HashMap keys:", test.keys());
+
+// values()
+// console.log("HashMap values:", test.values());
+
+// entries()
+// console.log("HashMap entries:", test.entries());
